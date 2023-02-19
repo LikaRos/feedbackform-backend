f5ca84146c04e19a83432adb2f36fec90677af14
Lika <lika.rossinskaya@gmail.com>
2023-02-19 17:40:32 +0200

    final  version

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  await app.listen(process.env.PORT);
+  await app.listen(3001);
 }
 bootstrap();
