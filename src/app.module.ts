148c3bc650c60174fd523f629b8515926b69976c
Lika <lika.rossinskaya@gmail.com>
2023-02-19 12:57:16 +0200

    port edited

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,6 @@ import { UserEntity } from '@app/user/user.entity';
       useFactory: () => ({
         type: 'mysql',
         host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT) || 3306,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
